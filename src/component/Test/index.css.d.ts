d3e274f28f20948a35dae907c3fde2e739cc198e
webgzh907247189 <18255106634@163.com>
2019-05-07 00:43:56 +0800

    999

--- src/component/Test/index.css.d.ts
+++ src/component/Test/index.css.d.ts
@@ -1,5 +1,5 @@
-export const footer: string
-export const item: string
+export const footer: string;
+export const item: string;
 export const content: string
 export const green: string
 export const red: string
