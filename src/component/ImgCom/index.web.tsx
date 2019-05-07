5bb0b835ee040cd44a618432837f826556d290f1
webgzh907247189 <18255106634@163.com>
2019-05-07 09:59:29 +0800

    update alt

--- src/component/ImgCom/index.web.tsx
+++ src/component/ImgCom/index.web.tsx
@@ -2,13 +2,13 @@ import * as React from 'react'
 
 export default class ImgCom extends React.Component<any, any> {
   componentWillMount() {}
-
-  componentWillReceiveProps(nextProps) {}
-
   render() {
     return (
       <div className="">
-        <img src="https://bztic-casaba.oss-cn-shanghai.aliyuncs.com/sit/88000785/dfc5fe60-ae46-441f-8fcc-bdb991892bea.png" />
+        <img
+          src="https://bztic-casaba.oss-cn-shanghai.aliyuncs.com/sit/88000785/dfc5fe60-ae46-441f-8fcc-bdb991892bea.png"
+          alt="图片"
+        />
       </div>
     )
   }
