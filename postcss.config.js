85aab79496e569bb5ab5a269bb8b1d8cbad1c7a4
webgzh907247189 <18255106634@163.com>
2019-05-09 18:50:59 +0800

    postcss 修正 支持最新css  cssnano css autopredixer

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,9 @@ module.exports = {
 		require('precss'),
 		require('postcss-preset-env'),
         require('autoprefixer')({browsers: ['last 10 versions','Firefox >= 20','>0%']}),
-        require('cssnano'),
     	require('postcss-pxtorem')({
     		rootValue: 75,propList: ['*','!font-size'],minPixelValue: 1 
         }),
-        
+        require('cssnano')
 	]
 }
